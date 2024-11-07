384b106ac6e6aca1869620cc4cbfe6aea578135b
Peter Odero <peterodero561@gmail.com>
2024-11-07 16:08:55 +0300

    encrypting password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Encrypt user set passwords'''
+import bycrpt
+
+def hash_password(password: str) -> bytes:
+    '''function to encrypt password and return salted hashed password'''
+    # salt password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
