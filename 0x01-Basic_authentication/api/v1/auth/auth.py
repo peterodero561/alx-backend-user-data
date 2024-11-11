ce75edda62fb8592d475be94ce5302bdc72fc192
Peter Odero <peterodero561@gmail.com>
2024-11-11 20:33:15 +0300

    base of authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Class for authentication for users'''
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    '''Used for authentication of users'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
