28905307ebbc07b96cf2732ed5c2d529d61e9c98
Peter Odero <peterodero561@gmail.com>
2024-11-13 11:08:13 +0300

    fix: adding documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,10 +7,13 @@ from typing import List, TypeVar
 class Auth:
     '''Used for authentication of users'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''check for authorization inpath'''
         return False
 
     def authorization_header(self, request=None) -> str:
+        '''check for authorization in header of path'''
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''check ceurrent user'''
         return None
