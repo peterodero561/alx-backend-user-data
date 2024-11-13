8b85d920f6fe50ebbb92fa67352ae503d31b856f
Peter Odero <peterodero561@gmail.com>
2024-11-13 13:12:59 +0300

    making BasicAuth a child of Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''Class for basic authentication'''
+from api.v1.auth.auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     '''class to be ussed in basic authentication'''
     pass
