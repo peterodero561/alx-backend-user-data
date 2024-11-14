7334495dd1fe0ca88239d0dc5b607cf95eb27896
Peter Odero <peterodero561@gmail.com>
2024-11-14 18:36:34 +0300

    build: created SessionAuth class for session authorization

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Class for session authenntication'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    '''Class to hold methods for session authentication'''
+    pass
