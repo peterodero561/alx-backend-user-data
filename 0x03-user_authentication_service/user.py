d054b823eeb3960364c4344b9f6fd14a44fde4db
Peter Odero <peterodero561@gmail.com>
2024-11-24 06:48:57 +0300

    feat: added the initialization method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,12 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     '''declares the format of the users table in database'''
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
+
+    def __init__(email, hashed_password):
+        self.email = email
+        self.hashed_password = hashed_password
